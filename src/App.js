2c899ff18679ff1c2cb8d465ee493c5fd202a64c
Mock1ng <bayuw1123@gmail.com>
2020-08-16 16:00:25 +0700

    fetch data, add font

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [flagsData, setFlagsData] = useState([]);
+
+  useEffect(() => {
+    getApi()
+  }, []);
+
+  const getApi = async () => {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const data = await res.json();
+    setFlagsData(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello, home!</h1>
+
+      <div className="flag-wrapper">
+        {flagsData.map(flag => (
+          <div key={flag.name} className="flag-card">
+            <div className="flag-container">
+              <img src={flag.flag} alt={flag.name + '\'s flag'} />
+            </div>
+            <div className="card-body">
+              <h4>{flag.name}</h4>
+              <p>Population: {flag.population}</p>
+              <p>Region: {flag.region}</p>
+              <p>Capital: {flag.capital}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
